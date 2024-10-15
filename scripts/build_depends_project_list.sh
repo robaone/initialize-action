9c1e25245e7de8687e7c20716778ce2e1483ad38
Ansel Robateau <robaone@gmail.com>
2024-10-15 23:59:27 +0000

    feat(PD-12): support empty root project folder

--- scripts/build_depends_project_list.sh
+++ scripts/build_depends_project_list.sh
@@ -17,7 +17,11 @@ function git_root() {
 }
 
 function list_projects_with_depends_file() {
-  PROJECTS_FOLDER="$(git_root)/$PROJECT_ROOT"
+  if [ "$PROJECT_ROOT" == "" ] || [ "$PROJECT_ROOT" == "." ]; then
+    PROJECTS_FOLDER="$(git_root)"
+  else
+    PROJECTS_FOLDER="$(git_root)/$PROJECT_ROOT"
+  fi
   for f in $($FIND_PATH $PROJECTS_FOLDER -name .depends)
   do
     echo $(basename $(dirname $f))
@@ -29,7 +33,12 @@ for project in $(list_projects_with_depends_file)
 do
   for file in $FILES
   do
-    for depends_path in $($CAT_PATH "$(git_root)/$PROJECT_ROOT/$project/.depends")
+    if [ "$PROJECT_ROOT" == "" ] || [ "$PROJECT_ROOT" == "." ]; then
+      DEPENDS_FILE_PATH="$(git_root)/$project/.depends"
+    else
+      DEPENDS_FILE_PATH="$(git_root)/$PROJECT_ROOT/$project/.depends"
+    fi
+    for depends_path in $($CAT_PATH "$DEPENDS_FILE_PATH")
     do
       DEPENDS_PATH_PATTERN=$(echo $depends_path | sed 's/\./\\./g' | sed 's/\*/.*/g')
       if [[ $file =~ $DEPENDS_PATH_PATTERN ]]; then
